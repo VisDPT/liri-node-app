d60f1f4b3adeebe40ccc183e55bc34648977f8f8
Vis Vasanth <viscodesdpt@gmail.com>
2016-10-04 19:33:53 -0500

    omdb formated and works

--- liri.js
+++ liri.js
@@ -1,6 +1,6 @@
 //grabs the keys from keys.js
-//var twitterKeyFile = require('./keys.js');
-//var twitterKeyInfo = twitterKeyFile.twitterKeys;
+var twitterKeyFile = require('./keys.js');
+var twitterKeyInfo = twitterKeyFile.twitterKeys;
 
 // Include the request npm package (Don't forget to run "npm install request" in this folder first!)
 
@@ -31,18 +31,20 @@ function omdbiRequest(){
 	request(queryUrl, function (error, response, body) {
 		// If the request is successful (i.e. if the response status code is 200)
 		if (!error && response.statusCode == 200) {
+
 			console.log(
-				"\n ==========================================" +
+				"\n==========================================" +
 				"\nMovie Title: "+ JSON.parse(body)["Title"]+
+				"\n"+
 				"\nYear Came Out: "+ JSON.parse(body)["Year"]+
 				"\nIMBDB rating: " + JSON.parse(body)["imdbRating"]+
 				"\nCountry of Production: " +JSON.parse(body)["Country"]+
-				"\nLanguage: " + JSON.parse(body)["Language"]+
+				"\nLanguage: " + JSON.parse(body)["Language"]+ "\n"+
 				"\nPlot: " + JSON.parse(body)["Plot"]+
-				"\nActors: " + JSON.parse(body)["Actors"]+
+				"\nActors: " + JSON.parse(body)["Actors"]+"\n"+
 				"\nRotten Tomatoes Rating: " + JSON.parse(body)["tomatoRating"]+
-				" Rotten Tomatoes URL: " + JSON.parse(body)["tomatoURL"]+
-				"\n =========================================="
+				"\nRotten Tomatoes URL: " + JSON.parse(body)["tomatoURL"]+
+				"\n=========================================="
 				)
 		}
 	});
