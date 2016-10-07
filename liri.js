dc54fa68d2580a874231a4be49304c33ea758fdf
Vis Vasanth <viscodesdpt@gmail.com>
2016-10-07 17:14:59 -0500

    DoWhatItSays Works woohoo

--- liri.js
+++ liri.js
@@ -5,7 +5,6 @@
 var Twitter = require('twitter');
 var twitterFile = require('./keys.js');
 var keys = new Twitter(twitterFile.twitterKeys);
-
 var params = {
     screen_name: 'VisDPT',
     count: 20,
@@ -16,21 +15,19 @@ var params = {
 function twitterFeed() {
     keys.get('statuses/user_timeline', params, function(error, tweets) {
         if (!error) {
-            for (var i=0; i<20; i++){
-            console.log('Tweet: ' + '"' + tweets[i].text + '"          ' + tweets[0].created_at);
+            for (var i = 0; i < 20; i++) {
+                console.log('Tweet: ' + '"' + tweets[i].text + '"          ' + tweets[0].created_at);
             }
         } else {
             console.log(error);
         }
     });
 }
-
 //====================SPOTIFY API from INQUIRER===============================================
 var SpotifyWebApi = require('spotify-web-api-node');
 var spotifyApi = new SpotifyWebApi();
 var spotifyArgs = process.argv;
 var songName = "";
-
 
 function spotifySong() {
     for (var i = 3; i < spotifyArgs.length; i++) {
@@ -40,7 +37,6 @@ function spotifySong() {
             songName = songName + spotifyArgs[i];
         }
     }
-
     spotifyApi.searchTracks('track:' + songName)
         .then(function(data) {
             console.log("Search Results for:  " + songName + "\n")
@@ -65,11 +61,9 @@ function spotifySong() {
             console.error(err);
         });
 }
-
 //data.body.tracks.items. 
 
 function spotifyNoSong() {
-
     spotifyApi.searchTracks("artist:" + "Ace of Base")
         .then(function(data) {
             console.log("\n" + "ARTIST:" + data.body.tracks.items[1].artists[0].name + ";" +
@@ -80,11 +74,7 @@ function spotifyNoSong() {
         }, function(err) {
             console.error(err);
         });
-
 }
-
-
-
 //=============================REQUEST OMDB API ===============================================
 var request = require('request');
 var nodeArgs = process.argv;
@@ -120,13 +110,28 @@ function omdbiRequest() {
         }
     });
 }
-
 var mrNobody = "http://www.imdb.com/title/tt0485947/";
 var mrNobodyMessage = "If you haven't watched 'Mr. Nobody', then you should: " +
     mrNobody +
     " It's on Netflix!"
-
-
+//============================== Do What It says ==================================
+var fs = require('fs');
+function doWhatItSays_BBB() {
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        var dataArr = data.split(',');
+        var command = dataArr[0];
+        var song = dataArr[1];
+        spotifyApi.searchTracks("track:" + song)
+            .then(function(data) {
+                console.log("\n" + "ARTIST:" + data.body.tracks.items[0].artists[0].name + ";" +
+                    "\n" + "SONG NAME:" + data.body.tracks.items[0].name + ";" +
+                    "\n" + "LINK:" + data.body.tracks.items[0].external_urls.spotify + ";" +
+                    "\n" + "Album Name:" + data.body.tracks.items[0].album.name + ";");
+            }, function(err) {
+                console.error(err);
+            });
+    });
+}
 //================================= PROCESS =========================================
 
 
@@ -151,5 +156,5 @@ if (process.argv[2] == "my-tweets") {
     }
     // ==========Do what it Says COMMAND =========
 } else if (process.argv[2] == "do-what-it-says") {
-    console.log("do what it says");
+    doWhatItSays_BBB();
 }
